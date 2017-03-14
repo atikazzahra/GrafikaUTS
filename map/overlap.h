9da3381adeedd6557f8876afed34b2fa011bc43f
stefanusagus <stefanusagusharyono@gmail.com>
2017-03-14 23:21:19 +0700

    Overlap done

--- map/overlap.h
+++ map/overlap.h
@@ -13,7 +13,7 @@ class Overlap : public Framebuffer {
 	private:
 		Framebuffer fb;
 		int m[600][600][3];
-		int i, j;
+		int i, j,k;
 
 	public:
 		Overlap() {
@@ -63,7 +63,28 @@ class Overlap : public Framebuffer {
 			
 			//front layer
 			//S
-			
+			for (i=83;i<=117;i++){
+				for (j=261;j<=270;j++){
+					m[i][j][2] = COLOR_3;
+				}
+				for (j=326;j<=335;j++){
+					m[i][j][2] = COLOR_3;
+				}
+				for (j=296;j<=305;j++){
+					m[i][j][2] = COLOR_3;
+				}
+			}
+			for (j=261;j<=301;j++){
+				for (i=83;i<=92;i++){
+					m[i][j][2] = COLOR_3;
+				}
+			}
+			for (j=301;j<=336;j++){
+				for (i=108;i<=117;i++){
+					m[i][j][2] = COLOR_3;
+				}
+			}
+
 			//U
 			for(j=261;j<=336;j++) {
 				for(i=156;i<=165;i++) {
@@ -80,8 +101,58 @@ class Overlap : public Framebuffer {
 			}
 			
 			//R
-			
+			for (i=231;i<=264;i++){
+				for (j=261;j<=270;j++){
+					m[i][j][2] = COLOR_3;
+				}
+				for (j=296;j<=305;j++){
+					m[i][j][2] = COLOR_3;
+				}
+			}
+			for (j=261;j<=336;j++){
+				for (i=231;i<=240;i++){
+					m[i][j][2] = COLOR_3;
+				}
+			}
+			for (j=261;j<=301;j++){
+				for (i=255;i<=264;i++){
+					m[i][j][2] = COLOR_3;
+				}
+			}
+			//GODDAMN DIAGONAL
+			//mulai di posisi tengah j=296
+			for (int k=0;k<10;k++){
+				i=231+k;
+				for (j=296;j<=336;j++){
+					m[i][j][2] = COLOR_3;
+					if(j%2==0){
+						i++;
+					}
+					else
+					if(j%3==0)
+					{
+						i++;
+					}
+					
+				}	
+			}
+
+
+
 			//V1
+			k=0;
+			for (j=261;j<=336;j++){
+				for (i=298+k;i<=308+k;i++){
+					m[i][j][2] = COLOR_3;
+				}
+				for (i=336-k;i>=326-k;i--){
+					m[i][j][2] = COLOR_3;
+				}
+				if (j%5==0){
+					k++;
+				}
+			}
+
 			
 			//I
 			for(i=369;i<=378;i++) {
@@ -91,7 +162,21 @@ class Overlap : public Framebuffer {
 			}
 			
 			//V2
-			
+			//V1
+			k=0;
+			for (j=261;j<=336;j++){
+				for (i=414+k;i<=424+k;i++){
+					m[i][j][2] = COLOR_3;
+				}
+				for (i=452-k;i>=442-k;i--){
+					m[i][j][2] = COLOR_3;
+				}
+				if (j%5==0){
+					k++;
+				}
+			}
+
+
 			//E
 			for(j=261;j<=336;j++) {
 				for(i=484;i<=494;i++) {
