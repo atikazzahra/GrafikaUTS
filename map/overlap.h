ec960c63921ecf8183def3fd23aa48849f0b5d82
Resa Kemal Saharso <resakemal@gmail.com>
2017-03-14 22:46:42 +0700

    Overlap update

--- map/overlap.h
+++ map/overlap.h
@@ -2,6 +2,8 @@
 #define OVERLAP_H
 
 #define COLOR_1 1
+#define COLOR_2 2
+#define COLOR_3 3
 #define EMPTY 0
 
 #include "framebuffer.h"
@@ -21,12 +23,6 @@ class Overlap : public Framebuffer {
 					for(k=0;k<=2;k++) {
 						m[i][j][k] = 0;
 					}
-				}
-			}
-			for(i=0;i<600;i++) {
-				for(j=0;j<600;j++) {
-					//if (m[i][j][1] != 0)
-						fb.putPixel(i,j,255,0,0); //sementara merah; nanti pake constant aja
 				}
 			}
 		}
@@ -61,7 +57,7 @@ class Overlap : public Framebuffer {
 			//Box
 			for(i=0;i<=600;i++) {
 				for(j=245;j<=351;j++) {
-					m[i][j][1] = COLOR_1;
+					m[i][j][1] = COLOR_2;
 				}
 			}
 			
@@ -71,10 +67,15 @@ class Overlap : public Framebuffer {
 			//U
 			for(j=261;j<=336;j++) {
 				for(i=156;i<=165;i++) {
-					m[i][j][2] = COLOR_1;
+					m[i][j][2] = COLOR_3;
 				}
 				for(i=183;i<=192;i++) {
-					m[i][j][2] = COLOR_1;
+					m[i][j][2] = COLOR_3;
+				}
+			}
+			for(i=165;i<=183;i++) {
+				for(j=329;j<=336;j++) {
+					m[i][j][2] = COLOR_3;
 				}
 			}
 			
@@ -85,7 +86,7 @@ class Overlap : public Framebuffer {
 			//I
 			for(i=369;i<=378;i++) {
 				for(j=261;j<=336;j++) {
-					m[i][j][2] = COLOR_1;
+					m[i][j][2] = COLOR_3;
 				}
 			}
 			
@@ -94,18 +95,18 @@ class Overlap : public Framebuffer {
 			//E
 			for(j=261;j<=336;j++) {
 				for(i=484;i<=494;i++) {
-					m[i][j][2] = COLOR_1;
+					m[i][j][2] = COLOR_3;
 				}
 			}
 			for(i=494;i<=518;i++) {
-				for(j=261;j<=276;j++) {
-					m[i][j][2] = COLOR_1;
+				for(j=261;j<=271;j++) {
+					m[i][j][2] = COLOR_3;
 				}
-				for(j=291;j<=306;j++) {
-					m[i][j][2] = COLOR_1;
+				for(j=294;j<=303;j++) {
+					m[i][j][2] = COLOR_3;
 				}
-				for(j=321;j<=336;j++) {
-					m[i][j][2] = COLOR_1;
+				for(j=326;j<=336;j++) {
+					m[i][j][2] = COLOR_3;
 				}
 			}
 		}
@@ -115,31 +116,40 @@ class Overlap : public Framebuffer {
 			for(i=0;i<=600;i++) {
 				for(j=0;j<=600;j++) {
 					if (m[i][j][2] != 0) {
-						if (m[i][j][2] == COLOR_1) {
-							fb.putPixel(i,j,255,0,0); //sementara merah; nanti pake constant aja
-						//add additional cases here
-					}
-				}
-			}
-
-			//print middle layer
-			for(i=0;i<=600;i++) {
-				for(j=0;j<=600;j++) {
-					if (m[i][j][1] != 0 && m[i][j][2] == 0) {
-						if (m[i][j][1] == COLOR_1) {
-							fb.putPixel(i,j,255,0,0); //idem
-						}
+						if (m[i][j][2] == COLOR_1) 
+							fb.putPixel(i+100,j+100,255,0,0);
+						else if (m[i][j][2] == COLOR_2) 
+							fb.putPixel(i+100,j+100,0,255,0);
+						else if (m[i][j][2] == COLOR_3) 
+							fb.putPixel(i+100,j+100,0,0,255);
 					}
 				}
 			}
 			
-			//print front layer
+			//print middle layer
+			for(i=0;i<=600;i++) {
+				for(j=0;j<=600;j++) {
+					if (m[i][j][1] != 0 && m[i][j][2] == 0) {
+						if (m[i][j][1] == COLOR_1)
+							fb.putPixel(i+100,j+100,255,0,0);
+						else if (m[i][j][1] == COLOR_2) 
+							fb.putPixel(i+100,j+100,0,255,0);
+						else if (m[i][j][1] == COLOR_3) 
+							fb.putPixel(i+100,j+100,0,0,255);
+					}
+				}
+			}
+			
+			//print back layer
 			for(i=0;i<=600;i++) {
 				for(j=0;j<=600;j++) {
 					if (m[i][j][0] != 0 && m[i][j][1] == 0) {
-						if (m[i][j][0] == COLOR_1) {
-							fb.putPixel(i,j,255,0,0); //idem
-						}
+						if (m[i][j][0] == COLOR_1)
+							fb.putPixel(i+100,j+100,255,0,0);
+						else if (m[i][j][0] == COLOR_2) 
+							fb.putPixel(i+100,j+100,0,255,0);
+						else if (m[i][j][0] == COLOR_3) 
+							fb.putPixel(i+100,j+100,0,0,255);
 					}
 				}
 			}
