0c53f60217d802494b1e78ad74ad555f6594939c
gangsaur <andrihardono@yahoo.com>
2017-03-14 22:48:13 +0700

    bug fix

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 sudo ./intro
 sudo ./rolling-game
 OUT=$?
-if [$OUT -eq 0];
+if [ $OUT -eq 0 ];
 then
 	sudo ./trans1
 	sudo ./shooter-game
 	OUT=$?
-	if [$OUT -eq 0];
+	if [ $OUT -eq 0 ];
 	then
 		sudo ./trans2
 		sudo ./map-game
 		OUT=$?
-		if [$OUT -eq 0];
+		if [ $OUT -eq 0 ];
 		then
 			sudo ./finish
 		else
